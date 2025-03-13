24431da2af986facd1562cb1a474fee52095c373
hualayn <layn@jinkuni.com>
2025-03-13 23:08:31 +0800

    update

--- src/bullets.py
+++ src/bullets.py
@@ -3,8 +3,8 @@ import pygame
 class Bullet(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.Surface((10, 10))
+        self.image = pygame.Surface((10, 10)).fill((255, 255, 255))
         self.rect = self.image.get_rect()
 
-    def draw(self, screen: pygame.Surface):
-        screen.blit(self.rect)
+    def update(self):
+        ...
--- src/player.py
+++ src/player.py
@@ -1,28 +1,50 @@
 import pygame
+from src.bullets import Bullet
 from .settings import SCREEN_WIDTH
 
 class Alien(pygame.sprite.Sprite):
     def __init__(self, *groups):
         super().__init__(*groups)
         alien_frame_folder = 'pics/charactors/AlienSprites/'
-        self.walk = [
+        self.walk_frames = [
             pygame.image.load(f'{alien_frame_folder}alienBlue_walk{i}.png').convert_alpha() for i in [1,2]
-        ]
+        ]        
+        self.jump_frame = pygame.image.load('pics/charactors/AlienSprites/alienBlue_jump.png').convert_alpha()
+        self.stand_frame = pygame.image.load('pics/charactors/AlienSprites/alienBlue_stand.png').convert_alpha()
         self.index = 0
-        self.image = self.walk[self.index]
-        self.jump = pygame.image.load('pics/charactors/AlienSprites/alienBlue_jump.png').convert_alpha()
+        self.image = self.walk_frames[self.index]
         self.rect = self.image.get_rect(midbottom=(200, 615))
         self.gravity = 0
         self.speed = 10
+        self.status = None
 
-    def player_input(self) -> None:
+    def walk(self) -> None:
+        self.index += 0.1
+        if self.index >= len(self.walk_frames):
+            self.index = 0
+        self.image = self.walk_frames[int(self.index)]
+
+    def stand(self) -> None:
+        self.image = self.stand_frame
+
+    def jump(self) -> None:
+        self.image = self.jump_frame
+
+    def shoot(self) -> None:
+        # self.bullets.add(Bullet())
+        ...
+
+    def player_input(self) -> str:
         keys = pygame.key.get_pressed()
         if keys[pygame.K_SPACE] and self.rect.y == 520:
             self.gravity = -20
         if keys[pygame.K_a]:
             self.rect.x = max(0, self.rect.x - self.speed)
+            return 'left'
         if keys[pygame.K_d]:
             self.rect.x = min(SCREEN_WIDTH - 100, self.rect.x + self.speed)
+            return 'right'
+        return 'stand'
 
     def apply_gravity(self) -> None:
         self.gravity += 1
@@ -30,16 +52,16 @@ class Alien(pygame.sprite.Sprite):
         if self.rect.y >= 520:
             self.rect.y = 520
 
-    def animation_state(self) -> None:
-        if self.rect.y == 520:
-            self.index += 0.1
-            if self.index >= len(self.walk):
-                self.index = 0
-            self.image = self.walk[int(self.index)]
+    def animation_state(self, status: str) -> None:
+        if self.rect.y < 520:
+            self.jump()
         else:
-            self.image = self.jump
+            if status == 'stand':
+                self.stand()
+            elif status in('left', 'right'):
+                self.walk()
 
     def update(self):
-        self.player_input()
+        status = self.player_input()
         self.apply_gravity()
-        self.animation_state()
+        self.animation_state(status)
